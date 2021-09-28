d58ebfe80bdbef1546a3cdd1cc691f8f52b69b79
Kristina <kristina07770@gmail.com>
2021-09-28 22:03:15 +0600

    minor update

--- src/App.tsx
+++ src/App.tsx
@@ -3,74 +3,46 @@ import './App.css';
 
 const App = () => {
 
-    const [boards, setBoards] = useState([
+    const [boards, setBoards] = useState<({id: number, title: string, items: {id: number, title: string}[]})[]>([
         {id: 1, title: 'TODO', items: [{id: 1, title: 'create react app'},{id: 2, title: 'go to sleep'}]},
-        {id: 2, title: 'In Progress', items: [{id: 0, title: ''}]},
-        {id: 3, title: 'Testing', items: [{id: 0, title: ''}]},
-        {id: 4, title: 'Done', items: [{id: 0, title: ''}]},
+        {id: 2, title: 'In Progress', items: []},
+        {id: 3, title: 'Testing', items: []},
+        {id: 4, title: 'Done', items: []},
     ]);
 
-    const [currentBoard, setCurrentBoard] = useState({
-        id: 0,
-        title: '',
-        items: [{id: 0, title: ''}],
-    });
-    const [currentItem, setCurrentItem] = useState({
-        id: 0,
-        title: '',
-    });
 
-    function dragOverHandler(e: React.DragEvent<HTMLDivElement>) {
-        e.preventDefault();
+    function changeTitle(board: { id: number; title: string; items: { id: number; title: string }[] } | { id: number; title: string; items?: undefined }, e: React.ChangeEvent<HTMLInputElement>) {
+        // setBoards((boards) => {
+        //     return boards.map((b) => (b.id === board.id)
+        //         ? ({
+        //             ...boards,
+        //             title: e.target.value,
+        //         })
+        //         : {...boards}
+        //     );
+        // })
     }
 
-
-    function dragLeaveHandler(e: React.DragEvent<HTMLDivElement>) {
-
-    }
-
-
-    function dragStartHandler(e:  React.DragEvent<HTMLDivElement>, board: { id: number; title: string; items: { id: number; title: string; }[]; }, item: { id: number; title: string; }) {
-        setCurrentBoard(board);
-        setCurrentItem(item);
-    }
-
-
-    function dragEndHandler(e: React.DragEvent<HTMLDivElement>) {
-    }
-
-
-    function dropHandler(e: React.DragEvent<HTMLDivElement>, board: { id: number; title: string; items: { id: number; title: string; }[]; }, item: { id: number; title: string; }) {
-        e.preventDefault();
-        const currentIndex = currentBoard.items.indexOf(currentItem);
-        currentBoard.items.splice(currentIndex, 1);
-        const dropIndex = board.items.indexOf(item);
-        board.items.splice(dropIndex + 1, 0, currentItem);
-        // @ts-ignore
-        setBoards(boards.map((b) => {
-            if (b.id === board.id) {
-                return board;
-            }
-        }))
-
+    function addItem() {
+        // setState, board.items.push()
     }
 
     return (
         <div className="app">
             {boards.map((board) => (
                 <div className="board">
-                    <div className="board-title">{board.title}</div>
+                    <div className="board-title">
+                        <input key={board.id} type="text" defaultValue={board.title} onChange={(e) => changeTitle(board, e)}/>
+                    </div>
                     {board.items.map((item) =>
-                        <div className="item" draggable={true}
-                             onDragOver={(e) => dragOverHandler(e)}
-                             onDragLeave={(e) => dragLeaveHandler(e)}
-                             onDragStart={(e) => dragStartHandler(e, board, item)}
-                             onDragEnd={(e) => dragEndHandler(e)}
-                             onDrop={(e) => dropHandler(e, board, item)}
-                        >
-                            {item.title}
-                        </div>
+                        <div className="item">{item.title}</div>
                     )}
+                    {board.items.length > 0 &&
+                        <div className="add-item" onClick={addItem}>
+                            <img src="/images/plus.svg" alt="add card"/>
+                            Добавить карточку
+                        </div>
+                    }
                 </div>
             ))}
         </div>
